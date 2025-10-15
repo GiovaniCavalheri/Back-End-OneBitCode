f18e27f40dc4d91820555aac20025403c2519dab
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2025-10-14 23:49:01 -0300

    finish-ex002

--- 00-Exercicios/001-Exercicio1/002 - Monitor-Sistema/index.js
+++ 00-Exercicios/001-Exercicio1/002 - Monitor-Sistema/index.js
@@ -1,56 +1,48 @@
 const os = require("node:os");
 const fs = require("node:fs");
 const path = require("node:path");
-const { time, log } = require("node:console");
-const { rejects } = require("node:assert");
-const { text } = require("node:stream/consumers");
 
 function viewData() {
   setInterval(() => {
     const version = os.version();
-
     const arch = os.arch();
-
     const model = os.cpus()[0].model;
-
     const timeSO = os.uptime();
-
     const memory = os.freemem();
-
     const totalMemory = os.totalmem();
-
     const usedMemory = totalMemory - memory;
 
-    function createFile(text) {
-      return new Promise((resolve, reject) => {
-        const logDir = path.join(__dirname, "log"); // Pasta 'log' no diretório atual
-        const filePath = path.join(logDir, "log.txt"); // Caminho completo do arquivo
+    const data = `Mostrando informações do Sistema:
+Versão do Sistema Operacional: ${version}
+Arquitetura do SO: ${arch}
+Modelo do Processador: ${model}
+Tempo de Atividade do Sistema (em minutos): ${(timeSO / 60).toFixed(2)}
+Uso de Memória: ${(usedMemory / 1024 / 1024 / 1024).toFixed(2)} GB
+-------------------------------
+`;
 
-        if (fs.existsSync(logDir)) {
-          console.log(`A pasta ${logDir} existe.`);
-        } else {
-          console.log(`A pasta ${logDir} não existe.`);
-          fs.mkdir(logDir);
-        }
-
-        fs.appendFile(filePath, text, (error) => {
-          if (error) {
-            reject(error.message);
-          } else {
-            resolve();
-          }
-        });
-      });
-    }
-
-    console.log(
-      `Mostrando infomações do Sistema: \n Versão do Sistema Operacional: ${version}\n Arquitetura do SO: ${arch}\n Modelo do Processador: ${model}\n Tempo de Atividade do Sistema(Em Minutos):  ${
-        timeSO / 60
-      }\n Uso de Memória: ${usedMemory / 1024 / 1024 / 1024} GB; \n`
-    );
-    
+    createFile(data);
   }, 1000);
 }
 
+function createFile(data) {
+  return new Promise((resolve, reject) => {
+    const logDir = path.join(__dirname, "log");
+    const filePath = path.join(logDir, "log.txt");
 
-viewData()
+    if (!fs.existsSync(logDir)) {
+      console.log(`A pasta ${logDir} não existe. Criando...`);
+      fs.mkdirSync(logDir);
+    }
+
+    fs.appendFile(filePath, data, (error) => {
+      if (error) {
+        reject(error.message);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+viewData();
